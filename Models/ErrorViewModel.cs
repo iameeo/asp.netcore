240b5eb9227c8b43ecb5610dd5444377217fe4a3
jaeho.lee <jaeho.lee@barunn.net>
2023-07-21 09:09:36 +0900

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
